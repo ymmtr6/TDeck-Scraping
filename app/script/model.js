d3aca34a81e13fc41acb5b91b6272a3c276fe836
Riku Yamamoto <ymmtr666@gmail.com>
2020-05-07 00:24:49 +0900

    BugFix

--- app/script/model.js
+++ app/script/model.js
@@ -1,27 +1,6 @@
 var mongoose = require("mongoose")
 var Schema = mongoose.Schema;
 
-var Tweet = new Schema({
-    id: { type: String, require: true, unique: true },
-    created_at: { type: Date, default: Date.now },
-    observed_at: { type: Date, default: Date.now },
-    full_text: String,
-    url: String,
-    source: String,
-    uid: {
-        type: { type: String },
-        ref: 'User'
-    },
-    in_reply_to_status_id: { type: String },
-    in_reply_to_user_id: { type: String },
-    in_reply_to_screen_name: String,
-    retweeted: Boolean,
-    is_quote_status: Boolean,
-    retweet_count: Number,
-    favorite_count: Number,
-
-});
-
 var User = new Schema({
     uid: { type: String, require: true, unique: true },
     name: String,
@@ -35,6 +14,28 @@ var User = new Schema({
     friends_count: Number,
     profile_image_url: String
 });
+
+var Tweet = new Schema({
+    id: { type: String, require: true, unique: true },
+    created_at: { type: Date, default: Date.now },
+    observed_at: { type: Date, default: Date.now },
+    full_text: String,
+    url: String,
+    source: String,
+    uid: {
+        type: String,
+        ref: 'User'
+    },
+    in_reply_to_status_id: { type: String },
+    in_reply_to_user_id: { type: String },
+    in_reply_to_screen_name: String,
+    retweeted: Boolean,
+    is_quote_status: Boolean,
+    retweet_count: Number,
+    favorite_count: Number,
+
+});
+
 
 exports.Tweet = mongoose.model("Tweet", Tweet);
 exports.User = mongoose.model("User", User);
